31ef5f34270aca80af15c6ad7082f475b8b68efd
Elgrogz <gregorgilchrist@gmail.com>
2017-03-11 12:43:47 +0000

    updated unit tests

--- tests/unit/calculator_spec.js
+++ tests/unit/calculator_spec.js
@@ -12,33 +12,29 @@ describe('calculator', function () {
   });
 
   it('can multiply 3x5 and get 15', function() {
-    calculator.numberClick(3);
-    calculator.operatorClick('*');
-    calculator.numberClick(5);
+    calculator.previousTotal = 3;
+    calculator.multiply(5);
     calculator.operatorClick('=');
     assert.equal(calculator.previousTotal, 15)
   });
 
   it('can divide 21/3 and get 7', function() {
-    calculator.numberClick(21);
-    calculator.operatorClick('/');
-    calculator.numberClick(3);
+    calculator.previousTotal = 21;
+    calculator.divide(3);
     calculator.operatorClick('=');
     assert.equal(calculator.previousTotal, 7)
   });
 
   it('can add 1+4 and get 5', function() {
-    calculator.numberClick(1);
-    calculator.operatorClick('+');
-    calculator.numberClick(4);
+    calculator.previousTotal = 1;
+    calculator.add(4);
     calculator.operatorClick('=');
     assert.equal(calculator.previousTotal, 5)
   });
 
   it('can subtract 7-4 and get 3', function() {
-    calculator.numberClick(7);
-    calculator.operatorClick('-');
-    calculator.numberClick(4);
+    calculator.previousTotal = 7;
+    calculator.subtract(4);
     calculator.operatorClick('=');
     assert.equal(calculator.previousTotal, 3)
   });
